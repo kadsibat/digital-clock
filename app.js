66770a0aa77ed43b50821d3025d4b9a3cbb77987
Batuhan <yesiltas_sibel@hotmail.com>
2022-03-05 12:23:10 +0300

    F

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
+// let hours= document.getElementById("hours");
+// let minutes  =document.getElementById("minutes");
+// let seconds=document.getElementById("seconds");
+// let pm=document.getElementById("pm");
+// // console.log(seconds);
 
+// const second=1000*60
+// const minute = second * 60;
+// const hour = minute * 60;
+// const day = hour * 24;
+
+
+
+
+
+// function cift(i) {
+//     if (i < 10) {i = "0" + i}
+//     return i;
+//   }
+  
+// setInterval(function(){
+
+//   pm.innerText=hours>=12 ? "PM":" "
+
+// },1000)
+
+
+  
 
 function Time(){
 
 
   
-  let cift= i=>i<10?"0"+i:i;
-    
- 
+  function cift(i) {
+    if (i < 10) {i = "0" + i}
+    return i;
+  }
+
   
   let hours= document.getElementById("hours");
   let minutes  =document.getElementById("minutes");
@@ -20,6 +49,9 @@ function Time(){
 
   if(d.getHours()<12){
     pm.innerText="AM"
+  }
+  else{
+    pm.innerText="PM"
   }
 
   setTimeout(Time, 1000);
